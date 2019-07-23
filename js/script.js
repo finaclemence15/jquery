3b37eb9338d1e5a23f7fdb26c0f2993e42bade40
finaclemence15 <finaclemence15@gmail.com>
2019-07-23 15:41:02 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
-jQuery('h1').click(function(){
-    alert("This is a heading tag")
-  });
+// jQuery('h1').click(function(){
+//     alert("This is a heading tag")
+//   });
 
-  jQuery('p').click(function(){
-    alert("This is a paragraph")
-  });
+//   jQuery('p').click(function(){
+//     alert("This is a paragraph")
+//   });
 
-  jQuery('img').click(function(){
-    alert("This is an image")
+//   jQuery('img').click(function(){
+//     alert("This is an image")
+//   });
+
+// jQuery(document).ready(function() {
+//     jQuery("h1").click(function() {
+//       alert("This is a header.");
+//     });
+  
+//     jQuery("p").click(function() {
+//       alert("This is a paragraph.");
+//     });
+  
+//     jQuery("img").click(function() {
+//       alert("This is an image.");
+//     });
+//   });
+// $(document).ready(function() {
+//     $("h1").click(function() {
+//       alert("This is a header.");
+//     });
+  
+//     $("p").click(function() {
+//       alert("This is a paragraph.");
+//     });
+  
+//     $("img").click(function() {
+//       alert("This is an image.");
+//     });
+//   });
+$(document).ready(function() {
+    $("p").click(function() {
+      $("img").show();
+    });
   });
