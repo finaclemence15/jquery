acbc724e883288d66aff18b8166b55da7b0af3de
finaclemence15 <finaclemence15@gmail.com>
2019-07-24 09:12:04 +0200

    adding function in js

--- js/script.js
+++ js/script.js
@@ -41,9 +41,22 @@
 //       $("img").show();
 //     });
 //   });
+  // $(document).ready(function() {
+  //   $("p").click(function() {
+  //     $(".walrus-showing").toggle();
+  //     $(".walrus-hidden").toggle();
+  //   });
+  // });
   $(document).ready(function() {
-    $("p").click(function() {
-      $(".walrus-showing").toggle();
-      $(".walrus-hidden").toggle();
+    $("button#green").click(function() {
+      $("body").addClass("green-background");
+    });
+
+    $("button#yellow").click(function() {
+      $("body").addClass("yellow-background");
+    });
+
+    $("button#red").click(function() {
+      $("body").addClass("red-background");
     });
   });
